6b5f0fe8425acb75d32278f8f191c2a803084a10
KaiV22 <kai.verleysen@gmail.com>
2024-08-14 17:41:16 +0200

    init backend

---
+++ backend/onitama2/src/main/java/com/example/onitama2/Onitama2Application.java
@@ -0,0 +1,13 @@
+package com.example.onitama2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Onitama2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Onitama2Application.class, args);
+    }
+
+}
---
+++ backend/onitama2/src/test/java/com/example/onitama2/Onitama2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onitama2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Onitama2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
